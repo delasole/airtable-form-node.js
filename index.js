d8741457db57e6fbe24fce437fc25f407c146348
donnishafrison <donnisha.frison@driveotto.com>
2019-10-28 17:51:23 -0500

    minor update.

---
+++ index.js
@@ -0,0 +1,5 @@
+const Person = require('./person');
+
+const person1 = new Person('Randall',29);
+
+person1.greeting();
---
+++ references/demo_path.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+// Base file name
+console.log(path.basename(__filename));
+
+// Directory name
+console.log(path.dirname(__filename));
+
+// File extension
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
